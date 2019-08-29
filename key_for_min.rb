5e6740ff8e5adccec2fde7f8669009fef5bd13be
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 06:00:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,6 @@ end
 def key_for_min_value(name_hash)
   hashes = {:indy => 500, :drag => 2, :circle_eights => 1}
   hashes.each do |track, rounds|
-    if hashes.select {|track, rounds| rounds == hashes.values.min }
-      puts hashes
-    end
+    hashes.invert.min&.last
   end
 end
