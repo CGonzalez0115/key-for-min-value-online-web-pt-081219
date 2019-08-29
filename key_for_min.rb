7dfa53b315303ad050cdce1ce3bed9f3c0d0dc38
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 05:57:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ end
 def key_for_min_value(name_hash)
   hashes = {:indy => 500, :drag => 2, :circle_eights => 1}
   hashes.each do |track, rounds|
-    if hashes.select {|track, rounds| rounds == hashes.values.max }
+    if hashes.select {|track, rounds| rounds == hashes.values.min }
       puts "#{rounds}"
     end
   end
