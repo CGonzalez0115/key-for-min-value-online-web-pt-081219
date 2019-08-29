bf5544a11d3ea22bb47b1ed5a2f12e1d008de222
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 07:08:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,16 +13,6 @@ def key_for_min_value(name_hash)
   hash
 end
 
-def key_for_min_value(name_hash)
-  name_hash = {:indy => 500, :drag => 2, :circle_eights => 1}
-  hash = ""
-  name_hash.each do |tracks, round|
-    puts name_hash.sort_by{ |tracks, round| round }.first
-    hash = tracks
-  end
-  hash
-end
-
 def key_for_min_value(name)
   name = Hash.new
   name[""]
