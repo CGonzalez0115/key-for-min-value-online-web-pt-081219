ca082b5bc4023f5d8d082bd10dbd45770e82853a
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 06:06:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,6 @@ end
 def key_for_min_value(name_hash)
   hashes = {:indy => 500, :drag => 2, :circle_eights => 1}
   hashes.each do |track, rounds|
-    hashes.invert.min&.last
+    hashes.values.min_by(&:last)
   end
 end
